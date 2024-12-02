26b685fc15e662ef236409f3339b156ebbba8db1
Grant <granther321@gmail.com>
2024-12-02 07:19:48 +0000

    testing 'resourcing' the packets when recieved from tunnel on server

--- server/server.go
+++ server/server.go
@@ -5,12 +5,17 @@ import (
 	"glorpn/types"
 	"log"
 	"net"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
 )
 
 type Server struct {
-	PublicIP net.IP
-	Iface    net.Interface
-	Key      string
+	PublicIP        net.IP
+	Iface           net.Interface
+	MainIfaceHandle *pcap.Handle
+	Key             string
 }
 
 func NewServer() types.Server {
@@ -18,20 +23,16 @@ func NewServer() types.Server {
 }
 
 func (s *Server) Start() error {
-	// Listen on main interface port 3000, if we get a data packet, send to main eth
-	// ip, err := getIfaceIP("tun0")
-	// if err != nil {
-	// 	return err
-	// }
-
-	// listener, err := net.Listen("tcp", net.JoinHostPort(ip.String(), "3000"))
-	// if err != nil {
-	// 	return err
-	// }
 	hostIP := "18.0.0.1"
 	listener, err := net.Listen("tcp", net.JoinHostPort(hostIP, "3000"))
 	if err != nil {
 		return err
+	}
+
+	// Create handle for main iface
+	s.MainIfaceHandle, err = pcap.OpenLive("eth0", 1600, true, pcap.BlockForever)
+	if err != nil {
+		log.Fatalf("Failed to create eth0 handle: %v", err)
 	}
 
 	for {
@@ -40,49 +41,23 @@ func (s *Server) Start() error {
 		if err != nil {
 			log.Fatalln("err while accept")
 		}
-		err = handle(conn)
+		err = s.handle(conn)
 		if err != nil {
 			fmt.Printf("Error handling packet: %v\n", err)
 		}
 	}
 }
 
-func getIfaceIP(ifaceName string) (net.IP, error) {
-	iface, err := net.InterfaceByName(ifaceName)
-	if err != nil {
-		return nil, err
-	}
-	// Server pub ip -> routes to servers tun iface:3000 -> Listen on this, if is data packet -> send to main iface
-
-	// listen on tun for traffic,
-	// incoming gets unwrapped and redirected to target iface
-	// start goroutine lisening for incoming
-	addrs, err := iface.Addrs()
-	if err != nil {
-		return nil, err
-	}
-
-	if len(addrs) < 1 {
-		return nil, fmt.Errorf("tun interface doesn't have an IP")
-	}
-
-	ip, _, err := net.ParseCIDR(addrs[0].String())
-	if err != nil {
-		return nil, err
-	}
-	return ip, nil
-}
-
 // What do I want?
 // Server: Exit node, client's traffic is routed throuhg here
 
-func handle(conn net.Conn) error {
+func (s *Server) handle(conn net.Conn) error {
 	fmt.Println("Connected to: ", conn.RemoteAddr())
 	buf := make([]byte, 2048)
 	for {
 		_, err := conn.Read(buf[:])
 		if err != nil {
-			return fmt.Errorf("err while reading from remote conn, closing conn and waiting again: %w\n", err)
+			return fmt.Errorf("err while reading from remote conn, closing conn and waiting again: %v", err)
 		}
 		packet := types.NewGlorpNPacket(buf[0], buf[1:len(buf)-1])
 		if packet.Header == 1 {
@@ -90,10 +65,51 @@ func handle(conn net.Conn) error {
 			sendAck(conn)
 		} else if packet.Header == 7 {
 			fmt.Println("Data Packet, data: ", string(packet.Data))
-			// sendMain(packet.Data)
+			newPack, err := s.resourcePacket(packet.Data)
+			if err != nil {
+				return err
+			}
+			err = s.MainIfaceHandle.WritePacketData(newPack)
+			if err != nil {
+				return err
+			}
 		}
 	}
 }
+
+func (s *Server) resourcePacket(data []byte) ([]byte, error) {
+	packet := gopacket.NewPacket(data, layers.LayerTypeEthernet, gopacket.Default)
+
+	ethLayer := packet.Layer(layers.LayerTypeEthernet)
+	ipLayer := packet.Layer(layers.LayerTypeIPv4)
+
+	if ethLayer == nil || ipLayer == nil {
+		return nil, fmt.Errorf("eth or ip layer was nil")
+	}
+
+	eth := ethLayer.(*layers.Ethernet)
+	ip := ipLayer.(*layers.IPv4)
+
+	newEth := *eth
+	newIP := *ip
+
+	newIP.SrcIP = net.IP{20, 0, 0, 2}
+
+	buffer := gopacket.NewSerializeBuffer()
+	opts := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
+
+	err := gopacket.SerializeLayers(buffer, opts, &newEth, &newIP)
+	if err != nil {
+		return nil, fmt.Errorf("failed to serialize layers")
+	}
+
+	return buffer.Bytes(), nil
+}
+
+// Whan data arrives at server, fix packet to be from src of server vpn IP and dst of main iface
 
 // Basic tunnel
 // Send data down it across the internet
